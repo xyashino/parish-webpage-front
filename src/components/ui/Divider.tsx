66e1efaf0848163fb3b4ac5652d26f18beb4450e
xyashino <szczypiorpan123@gmail.com>
2023-02-12 20:09:05 +0100

    Now code looks better

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -1,14 +1,16 @@
 import React, { HTMLAttributes } from "react";
+
 interface Props extends Omit<HTMLAttributes<HTMLDivElement>, "className"> {
   horizontal?: true;
   value?: string;
   className?: string;
 }
+
 export const Divider = ({ horizontal, value, className, ...props }: Props) => {
-  const tailwindStyles = `divider mb-0 ${
-    horizontal ? "divider-horizontal" : ""
-  } 
-      ${className ? className : ""}`;
+  const horizontalStyle = horizontal ? "divider-horizontal" : "";
+  const ownClasses = className ? className : "";
+  const tailwindStyles = `divider mb-0 ${horizontalStyle} ${ownClasses}`;
+
   return (
     <div className={tailwindStyles} {...props}>
       {value && ""}
