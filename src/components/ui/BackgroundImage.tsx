f02f6940914c278f82404c383c274df043e6b9eb
xyashino <szczypiorpan123@gmail.com>
2023-02-12 20:10:26 +0100

    Renamed imported image

--- src/components/ui/BackgroundImage.tsx
+++ src/components/ui/BackgroundImage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Divider } from "@components/ui/Divider";
-import Image from "@assets/default/backgroundImage.webp";
+import DefaultImage from "@assets/default/backgroundImage.webp";
 
 interface Props {
   image?: string;
@@ -12,9 +12,7 @@ export const BackgroundImage = ({ image, divider }: Props) => {
     <>
       <section
         className={`max-w-screen relative flex h-[30vh] w-full  bg-cover bg-scroll bg-center lg:h-[65vh]  lg:bg-fixed`}
-        style={{
-          backgroundImage: `url(${image ?? Image})`,
-        }}
+        style={{ backgroundImage: `url(${image ?? DefaultImage})` }}
       >
         <div className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-40" />
       </section>
