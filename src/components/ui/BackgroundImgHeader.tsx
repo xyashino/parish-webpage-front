fbe9135b45d85bbf213f1726c3c5b5c3dff266de
xyashino <szczypiorpan123@gmail.com>
2023-02-26 21:28:39 +0100

    Rename and move ArticleImgHeader component to BackgroundImgHeader."

---
+++ src/components/ui/BackgroundImgHeader.tsx
@@ -0,0 +1,23 @@
+import React, { PropsWithChildren } from "react";
+import { Divider } from "@components/ui/Divider";
+
+interface Props extends PropsWithChildren {
+  title: string;
+  divider?: true;
+}
+
+export const BackgroundImgHeader = ({ title, children, divider }: Props) => {
+  return (
+    <>
+      <div className="relative flex w-full flex-col items-center">
+        <div className="w-full bg-primary p-6 font-bold text-base-100 shadow lg:w-4/5 lg:-translate-y-1/2">
+          <h2 className="text:2xl m-0 uppercase text-base-100 lg:text-3xl">
+            {title}
+          </h2>
+          {children}
+        </div>
+        {divider ? <Divider /> : null}
+      </div>
+    </>
+  );
+};
