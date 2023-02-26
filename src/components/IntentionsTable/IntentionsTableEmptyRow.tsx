61dc4b758c4d824c9baf908afb8b0e94f7490993
xyashino <szczypiorpan123@gmail.com>
2023-02-26 21:32:31 +0100

    Add styles for IntentionsTableEmptyRow component.

--- src/components/IntentionsTable/IntentionsTableEmptyRow.tsx
+++ src/components/IntentionsTable/IntentionsTableEmptyRow.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const IntentionsTableEmptyRow = () => {
   return (
-    <tr>
+    <tr className="text-center font-bold">
       <td colSpan={2}> Upps... Brak intencji </td>{" "}
     </tr>
   );
