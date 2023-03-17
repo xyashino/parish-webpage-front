5c543a57f9573990e6c834b031f67155cd7acd0d
xyashino <szczypiorpan123@gmail.com>
2023-03-17 16:53:55 +0100

    Create ModalBackGround component for better modal structure

---
+++ src/components/ui/Modal/Modal.tsx
@@ -0,0 +1,18 @@
+import React, { PropsWithChildren } from "react";
+import { createPortal } from "react-dom";
+import { ModalBackGround } from "@components/ui/Modal/ModalBackground";
+
+interface Props extends PropsWithChildren {
+  boxModalClasses?: string;
+  hideModal: (e: any) => void;
+  showModal: boolean;
+}
+
+export const Modal = ({ children, hideModal, showModal }: Props) => {
+  const bodyElement = document.querySelector("body");
+  if (!bodyElement) return null;
+  const modal = (
+    <ModalBackGround hideModal={hideModal}>{children}</ModalBackGround>
+  );
+  return <>{showModal ? createPortal(modal, bodyElement) : null}</>;
+};
---
+++ src/components/ui/Modal/ModalBackground.tsx
@@ -0,0 +1,22 @@
+import React, { PropsWithChildren, SyntheticEvent, useRef } from "react";
+interface Props extends PropsWithChildren {
+  hideModal: (e: SyntheticEvent) => void;
+}
+
+export const ModalBackGround = ({ children, hideModal }: Props) => {
+  const modalRef = useRef(null);
+  const handleClick = (e: SyntheticEvent) => {
+    if (e.target === modalRef.current) {
+      hideModal(e);
+    }
+  };
+  return (
+    <div
+      className="fixed inset-0 z-40 flex flex-col items-center  bg-black/40  transition-all"
+      onClick={handleClick}
+      ref={modalRef}
+    >
+      {children}
+    </div>
+  );
+};
