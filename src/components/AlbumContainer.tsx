665d01309b0b26851df59c26673437af006a25a4
xyashino <szczypiorpan123@gmail.com>
2023-03-17 17:01:32 +0100

    Add AlbumContainer and AlbumCard components

---
+++ src/components/AlbumContainer.tsx
@@ -0,0 +1,50 @@
+import React, { useLayoutEffect, useState } from "react";
+import { AlbumTypeResponse } from "@backendTypes";
+import { AlbumCard } from "@components/ui/AlbumCard";
+import { getDataFrom } from "@utils/network.util";
+import { PageRouter } from "@enums/page-router.enum";
+
+interface Props {
+  id: string;
+}
+
+export const AlbumContainer = ({ id }: Props) => {
+  const [data, setData] = useState<null | AlbumTypeResponse>(null);
+
+  useLayoutEffect(() => {
+    (async () => {
+      setData(null);
+      setData(await getDataFrom(`/albums/types/${id}` as PageRouter));
+    })();
+  }, [id]);
+
+  if (!data) {
+    return <div className="loading mt-8 p-4 text-3xl">Loading...</div>;
+  }
+
+  if (data.albums.length === 0) {
+    return (
+      <div className="p-4">
+        <h2 className="mt-4 p-4 text-3xl">
+          Upps... Wygląda na to że nie ma jeszcze{" "}
+          <span className="font-bold uppercase text-error">żadnych</span>{" "}
+          albumów.
+        </h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-wrap  justify-around  p-4">
+      {data.albums.map(({ title, subtitle, id, backgroundImage }) => (
+        <AlbumCard
+          title={title}
+          subtitle={subtitle}
+          key={id}
+          id={id}
+          bgImg={backgroundImage}
+        />
+      ))}
+    </div>
+  );
+};
---
+++ src/components/ui/AlbumCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Album } from "@backendTypes";
+import { Link } from "react-router-dom";
+
+const { VITE_API_ALBUM_URL } = import.meta.env;
+
+interface Props {
+  title: Album["title"];
+  subtitle: Album["subtitle"];
+  bgImg: Album["backgroundImage"];
+
+  id: Album["id"];
+}
+
+export const AlbumCard = ({ title, subtitle, id, bgImg }: Props) => {
+  return (
+    <div className="max-w-5/6 card glass mb-8 w-96">
+      <figure>
+        <img src={VITE_API_ALBUM_URL + bgImg} alt="Album cover" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        {subtitle ? <p>{subtitle}</p> : null}
+        <div className="card-actions justify-end">
+          <Link to={`/album/${id}`} className="btn-primary btn">
+            Zobacz Więcej
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
